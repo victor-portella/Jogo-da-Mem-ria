0570c3a090f90eea3cd630e8c2e5613d0c4c1479
Victor Portella <victor.26.pimenta@gmail.com>
2022-09-14 18:05:50 -0300

    Adição da lógica do jogo

--- game.js
+++ game.js
@@ -1,4 +1,47 @@
 let game = {
+
+    lockMode: false,
+    firstCard: null,
+    secondCard: null,
+
+    setCard: function (id) {
+        let card = this.cards.filter(card => card.id === id)[0]
+
+        if (card.flipped || this.lockMode) {
+            return false
+        }
+
+        if (!this.firstCard) {
+            this.firstCard = card
+            this.firstCard.flipped = true
+            return true
+        } else {
+            this.secondCard = card
+            this.secondCard.flipped = true
+            this.lockMode = true
+            return true
+        }
+    },
+
+    checkMatch: function () {
+        if (!this.firstCard || !this.secondCard) {
+            return false
+        }
+        return this.firstCard.icon === this.secondCard.icon
+    },
+
+    unflipCards: function () {
+        this.firstCard.flipped = false
+        this.secondCard.flipped = false
+        this.clearCards()
+    },
+
+    clearCards: function () {
+        this.firstCard = null
+        this.secondCard = null
+        this.lockMode = false
+    },
+
     techs: [
         "bootstrap",
         "css",
--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const FRONT = "card_front"
 const BACK = "card_back"
 const CARD = "card"
 const ICON = "icon"
+const FLIP = "flip"
 
 startGame()
 
@@ -48,5 +49,23 @@ function createCardFace(face, card, element) {
 }
 
 function flipCard() {
-    this.classList.add("flip")
+    if (game.setCard(this.id)) {
+        this.classList.add(FLIP)
+
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards()
+            } else {
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id)
+                    let secondCardView = document.getElementById(game.secondCard.id)
+
+                    firstCardView.classList.remove(FLIP)
+                    secondCardView.classList.remove(FLIP)
+
+                    game.unflipCards()
+                }, 1000)
+            }
+        }
+    }
 }
