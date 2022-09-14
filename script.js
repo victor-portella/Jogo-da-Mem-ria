839c322c2af8f6c7bcc16e4d07719b721debdb1f
Victor Portella <victor.26.pimenta@gmail.com>
2022-09-14 16:31:48 -0300

    Separação da View em relação a lógica e criação do arquivo game.js

--- script.js
+++ script.js
@@ -3,27 +3,11 @@ const BACK = "card_back"
 const CARD = "card"
 const ICON = "icon"
 
-let techs = [
-    "bootstrap",
-    "css",
-    "electron",
-    "firebase",
-    "html",
-    "javascript",
-    "jquery",
-    "mongo",
-    "node",
-    "react"
-]
-
-let cards = null
-
 startGame()
 
 function startGame() {
-    cards = createCardsFromTechs(techs)
-    shuffleCards(cards)
-    initializeCards(cards)
+    game.createCardsFromTechs()
+    initializeCards(game.cards)
 }
 
 function initializeCards(cards) {
@@ -61,43 +45,6 @@ function createCardFace(face, card, element) {
     }
 
     element.appendChild(cardElementFace)
-}
-
-function shuffleCards(cards) {
-    let currentIndex = cards.length
-    let randomIndex = 0
-
-    while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex)
-        currentIndex--
-        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
-    }
-}
-
-function createCardsFromTechs(techs) {
-    let cards = []
-
-    techs.forEach((tech) => {
-        cards.push(createPairFromTech(tech))
-    })
-
-    return cards.flatMap(pair => pair)
-}
-
-function createPairFromTech(tech) {
-    return [{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false
-    }, {
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false
-    }]
-}
-
-function createIdWithTech(tech) {
-    return tech + parseInt(Math.random() * 1000)
 }
 
 function flipCard() {
